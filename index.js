f2002a1742ac92ee01c6b1c87320829a09d28516
faizahmaddae <faizahmad.daiee@gmail.com>
2024-04-13 20:42:22 -0400

    new update

--- index.js
+++ index.js
@@ -51,26 +51,11 @@ app.get('/api', async (req, res) => {
   //   return res.send(object);
   // }
 
-  // https://instaoffline.net/process/
-  // const result_1 = await _loadSite1(url);
-  // if (result_1.status == 'success') {
-  //   return res.send(result_1)
-  // }
-
-
-
-  //  // https://snapinsta.app/action.php
+  
   const result_3 = await _snapinsta(url);
   if (result_3.status == 'success') {
     return res.send(result_3)
   }
-
-  // https://reelit.io/api/fetch
-  const result_2 = await _loadSite2(url);
-  if (result_2.status == 'success') {
-    return res.send(result_2)
-  }
-
 
   return res.send(object);
 });
@@ -213,10 +198,8 @@ async function fetchCsrfTokenWithCookies(url, selector = 'meta[name="csrf-token"
     const response = await axios.get(url, { headers });
     const html = response.data;
 
-    const serverTime = response.headers['date'];
-    const timestamp = new Date(serverTime).getTime();
-
-    // console.log('Server Time:', timestamp);
+    // const serverTime = response.headers['date'];
+    // const timestamp = new Date(serverTime).getTime();
 
     // Use cheerio to parse the HTML
     const $ = cheerio.load(html);
@@ -234,104 +217,6 @@ async function fetchCsrfTokenWithCookies(url, selector = 'meta[name="csrf-token"
   }
 }
 
-
-
-async function _loadSite2(instaUrl) {
-  const siteUrl = 'https://reelit.io/api/fetch';
-
-  var object = {};
-  object['status'] = 'success';
-  object['error'] = null;
-  object['site'] = siteUrl;
-  object['data'] = [];
-
-  await axios.post(siteUrl, { url: instaUrl }, {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Accept': 'application/json, text/plain, */*',
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'en-US,en;q=0.9',
-      'Connection': 'keep-alive',
-      'Host': 'reelit.io',
-      'Origin': 'https://reelit.io',
-      'Referer': 'https://reelit.io/',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-    },
-  }).then((response) => {
-
-    const mediaList = response.data.media.data.mediaList;
-    for (let i = 0; i < mediaList.length; i++) {
-      const images = mediaList[i].images;
-      const videos = mediaList[i].videos;
-      const is_video = mediaList[i].contentType == 'video' ? true : false;
-
-      var url = null;
-
-      if (images.length > 0) {
-        url = images[images.length - 1].url;
-      }
-      if (videos != null && videos.length > 0) {
-        url = videos[videos.length - 1].url;
-      }
-
-      object['data'].push({ url: url, is_video: is_video });
-
-    }
-
-  }).catch((error) => {
-    console.log(`Error: ${siteUrl}`);
-    object['status'] = 'error';
-    object['error'] = 'No links found';
-    object['data'] = null;
-    object['error'] = error;
-  }
-  );
-
-  return object;
-
-}
-
-
-async function _loadSite1(instaUrl) {
-  const siteUrl = 'https://instaoffline.net/process/';
-
-  var object = {};
-  object['status'] = 'success';
-  object['error'] = null;
-  object['site'] = siteUrl;
-  object['data'] = [];
-
-
-  await axios.post(siteUrl, { q: instaUrl, downloader: 'image' }, {
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  }).then((response) => {
-
-    const dom = htmlparser2.parseDocument(response.data.html);
-    const $ = cheerio.load(dom);
-
-    const tag = 'div.items-list';
-
-    $(tag).find('div.item').each(function (i, elem) {
-      const url = $(elem).find('a').attr('href').replace('&dl=1', '');
-      const is_video = $(elem).find('span.fa-video').length > 0 ? true : false;
-      object['data'].push({ url: url, is_video: is_video });
-    });
-
-    if ($(tag).find('div.item') == 0) {
-      object['status'] = 'error';
-      object['error'] = 'No links found';
-      object['data'] = null;
-    }
-
-  }).catch((error) => {
-    console.log(`Error: ${siteUrl}`);
-    object['status'] = 'error';
-    object['error'] = 'No links found';
-    object['data'] = null;
-  });
-
-  return object;
-}
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}/api`)
